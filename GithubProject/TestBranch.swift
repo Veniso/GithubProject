ae549f39e5f5218a77cc7aafed41d14f1cbc660c
apple <apple@apples-macbook-pro.local>
2018-07-26 15:41:42 +0530

    added  new class

---
+++ GithubProject/TestBranch.swift
@@ -0,0 +1,13 @@
+//
+//  TestBranch.swift
+//  GithubProject
+//
+//  Created by apple on 26/07/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+
+class TestBranch: NSObject {
+
+}
